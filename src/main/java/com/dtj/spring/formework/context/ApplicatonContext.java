191f16895f8207f46dbf042aa74023f0fdeace40
邓天骄 <jiao_snow@126.com>
2018-12-27 07:48:26 +0800

    spring仿真版

--- src/main/java/com/dtj/spring/formework/context/ApplicatonContext.java
+++ src/main/java/com/dtj/spring/formework/context/ApplicatonContext.java
@@ -1,9 +1,14 @@
 package com.dtj.spring.formework.context;
 
+import com.dtj.spring.formework.context.support.BeanDefinitionReader;
 import com.dtj.spring.formework.core.BeanFactory;
+
+import java.util.List;
 
 public class ApplicatonContext implements BeanFactory {
     String[] configLocation;
+
+    private BeanDefinitionReader reader;
 
     public ApplicatonContext(String... locations){
         this.configLocation = locations;
@@ -12,6 +17,17 @@ public class ApplicatonContext implements BeanFactory {
 
     public void refresh(){
 
+//        定位
+        this.reader = new BeanDefinitionReader(configLocation);
+//        加载
+        List<String> beanDefinitions = reader.loadBeanDefinition();
+//        注册
+        doRegisty(beanDefinitions);
+//        依赖注入
+
+    }
+
+    private void doRegisty(List<String> beanDefinitions) {
     }
 
     @Override
--- src/main/java/com/dtj/spring/formework/context/support/BeanDefinitionReader.java
+++ src/main/java/com/dtj/spring/formework/context/support/BeanDefinitionReader.java
@@ -2,13 +2,21 @@ package com.dtj.spring.formework.context.support;
 
 import com.dtj.spring.formework.beans.BeanDefinition;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
+//用对配置文件进行查找，读取、解析
 public class BeanDefinitionReader {
 
     public Properties properties = new Properties();
+    private List<String> registyBeanClasses = new ArrayList<String>();
+    //在配置文件中，用来获取自动扫描的包名的key
+    private final String SCAN_PACKAGE = "scanPackage";
 
     public BeanDefinitionReader(String... locations){
         //spring中是用reader来做定位，这里直接
@@ -26,6 +34,7 @@ public class BeanDefinitionReader {
                 }
             }
         }
+        doScanner(SCAN_PACKAGE);
     }
 
     public BeanDefinition registerBean(String ClassName){
@@ -34,5 +43,28 @@ public class BeanDefinitionReader {
 
     public Properties getProperties(){
         return this.properties;
+    }
+
+    public List<String> loadBeanDefinition() {
+
+        return this.registyBeanClasses;
+    }
+
+    //递归扫描所有的相关联的class，并且保存到一个List中
+    private void doScanner(String packageName) {
+
+        URL url = this.getClass().getClassLoader().getResource("/" + packageName.replaceAll("\\.","/"));
+
+        File classDir = new File(url.getFile());
+
+        for (File file : classDir.listFiles()){
+            if(file.isDirectory()){
+                doScanner(packageName + "." +file.getName());
+            }else {
+                registyBeanClasses.add(packageName + "." + file.getName().replace(".class",""));
+            }
+        }
+
+
     }
 }
